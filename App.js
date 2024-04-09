3e99155da1d7f200e2d1b9574eb84ae119aab75b
Raju Dhoni <32018260+rajusiripalli@users.noreply.github.com>
2024-04-09 13:45:34 +0530

    videoplayer

---
+++ App.js
@@ -0,0 +1,11 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import CustomVideoPlayer from './src/CustomVideoPlayer';
+
+const App = () => {
+  return <CustomVideoPlayer />;
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
